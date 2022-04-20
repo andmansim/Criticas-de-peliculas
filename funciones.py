105f3666f6ef720b30d39973c74cf2e678d40599
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-20 18:48:29 +0200

    cambio nombres

--- funciones.py
+++ funciones.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 
 #creamos un DataFrame
-df = pd.DataFrame({'Xi': np.array([5, 4, 3, 2, 1, 0]), 'Ni': np.array([40, 99, 145, 133, 96, 40])})
+df = pd.DataFrame({'Valoración película': np.array([5, 4, 3, 2, 1, 0]), 'Número de votos': np.array([40, 99, 145, 133, 96, 40])})
 print(df)
