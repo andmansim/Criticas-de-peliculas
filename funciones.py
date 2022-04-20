db70986958197bbe68a38619ea5a99e05b49b105
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-20 18:43:38 +0200

    crear dataframe con los datos

--- funciones.py
+++ funciones.py
@@ -2,6 +2,5 @@ import pandas as pd
 import numpy as np
 
 #creamos un DataFrame
-data = {'Xi': [5, 4, 3, 2, 1, 0], 'Ni': [40, 99, 145, 133, 96, 40]}
-df = pd.DataFrame(data)
+df = pd.DataFrame({'Xi': np.array([5, 4, 3, 2, 1, 0]), 'Ni': np.array([40, 99, 145, 133, 96, 40])})
 print(df)
