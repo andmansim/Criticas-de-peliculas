5c746a2f96c10fcce0360831998128f67c0f65cc
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-20 19:04:27 +0200

    calculo la media

--- funciones.py
+++ funciones.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 #creamos un DataFrame
 df = pd.DataFrame({'Valoración película': np.array([5, 4, 3, 2, 1, 0]), 'Número de votos': np.array([40, 99, 145, 133, 96, 40])})
 print(df)
+#media
+media = ((df['Valoración película'] * df['Número de votos']).sum())/(df['Número de votos'].sum())
+print(media)
+
