b22d4d65c949740d6e3cb6752e5613ecd0f459b1
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-27 18:54:56 +0200

    intento 4

--- funciones.py
+++ funciones.py
@@ -70,8 +70,14 @@ plt.show()
 def representacion(df, desviacion_tipica, max, min, media, varianza):
     x = np.arange(min, max, 0.01)
     f = 1/(desviacion_tipica * np.sqrt(2*np.pi)) * np.exp(-(x - media) ** 2/(2 * varianza))
+    f1 = (np.exp((-1/2) * ((x-media)/desviacion_tipica)**2))/np.sqrt(2 * np.pi * x * desviacion_tipica)
+    
     plt.subplot(1,1,1)
-    fig, ax = plt.hist(df['Valoracion-pelicula']), plt.plot(x, f, color = 'black', linestyle = 'dashed')
+    fig = plt.hist(df['Valoracion-pelicula'])
+    plt.axvline(media, color='red', linestyle='dashed', linewidth=1,label = str(media))
+    plt.legend(loc='upper right')
+    b =  plt.plot(x, f1, color = 'red', linestyle = 'dashed')
+    a =  plt.plot(x, f, color = 'black', linestyle = 'dashed')
     
     '''  plt.subplot(1,1,1)
     plt.style.use('seaborn')'''
@@ -79,4 +85,4 @@ def representacion(df, desviacion_tipica, max, min, media, varianza):
     #plt.plot(x, f, color = 'black', linestyle = 'dashed')
     #axis = df.plot.bar(x='Valoracion-pelicula', rot = 0)
     plt.show()
-    
+
